83f6dc6fccaa64e3dee96ece0ff7b4dffcf8b245
navid-sheikh <navidsheikh2020@gmail.com>
2022-03-20 00:15:41 +0000

    Redis + Channel

--- .history/sentiapp/settings_20220320001238.py
+++ .history/sentiapp/settings_20220320001238.py
@@ -168,8 +168,7 @@ MEDIA_URL =  '/media/'
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # AUTH_USER_MODEL = 'mainapp.User'
 
-CELERY_BROKER_URL  =  os.environ['REDIS_URL']
-# CELERY_BROKER_URL  =  'redis://127.0.0.1:6379'
+CELERY_BROKER_URL  =  'redis://127.0.0.1:6379'
 CELERY_ACCEPT_CONTENT  =  ['application/json']
 CELERY_RESULT_SERIALIZER =  'json'
 CELERY_TASK_SERIALIZER =  'json'
